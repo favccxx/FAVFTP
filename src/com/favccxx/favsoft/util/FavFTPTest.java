3251452ba066ac8cd831b7a8ea8edd83db785beb
穿越来客 <chenxu.wo@163.com>
2015-08-03 22:03:01 +0800

    FAVFTPServer
    FileZilla Server and Java implements

---
+++ src/com/favccxx/favsoft/util/FavFTPTest.java
@@ -0,0 +1,21 @@
+package com.favccxx.favsoft.util;
+
+import junit.framework.TestCase;
+
+public class FavFTPTest extends TestCase {
+	
+	public void testFavFTPUtil(){
+		String hostname = "127.0.0.1";
+		int port = 21;
+		String username = "business";
+		String password = "business";
+		String pathname = "business/ebook"; 
+		String filename = "big.rar"; 
+		String originfilename = "C:\\Users\\chenxu3\\Downloads\\Downloads.rar";
+		FavFTPUtil.uploadFileFromProduction(hostname, port, username, password, pathname, filename, originfilename);
+//		String localpath = "D:/";
+		
+//		FavFTPUtil.downloadFile(hostname, port, username, password, pathname, filename, localpath);
+	}
+
+}
